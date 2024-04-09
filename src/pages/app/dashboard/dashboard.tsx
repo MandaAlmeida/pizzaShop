6402747f8e0d5ecf512dc65290111c1985935975
amanda almeida <manda.fonsecaff@gmail.com>
2024-04-09 20:47:10 -0300

    Adiociona pagina de dashboard

---
+++ src/pages/app/dashboard/dashboard.tsx
@@ -0,0 +1,26 @@
+import { Helmet } from "react-helmet-async";
+import { MonthRevenueCard } from "./monthRevenueCard";
+import { MonthOrdersAmountCard } from "./monthOrdersAmountCard";
+import { DayOrdersAmountCard } from "./dayOrdersAmountCard";
+import { MonthCanceledOrdersAmountCard } from "./monthCanceledOrdersCard";
+import { RevenueChart } from "./revenueChart";
+
+export function Dashboard() {
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tighter">Dashboard</h1>
+        <div className="grid grid-cols-4 gap-4">
+          <MonthRevenueCard />
+          <MonthOrdersAmountCard />
+          <DayOrdersAmountCard />
+          <MonthCanceledOrdersAmountCard />
+        </div>
+        <div className=" grid grid-cols-9 gap-4">
+          <RevenueChart />
+        </div>
+      </div>
+    </>
+  );
+}
